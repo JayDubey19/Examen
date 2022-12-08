e86ec1811c837797de6006fed5e627274d3afcda
intel <adityakhaladkar98@gmal.com>
2022-12-08 09:59:24 +0530

    added firebase auth

--- app/src/main/java/com/example/examen/LoginTabFragment.java
+++ app/src/main/java/com/example/examen/LoginTabFragment.java
@@ -1,4 +1,5 @@
 package com.example.examen;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -7,34 +8,57 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
-public class LoginTabFragment extends Fragment  {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginTabFragment extends Fragment {
 
     TextView forgetpass;
     EditText email;
-    EditText pass ;
+    EditText pass;
     Button login;
-    float v=0 ;
+    float v = 0;
+    FirebaseAuth firebaseAuth;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container , Bundle savedInstanceState)
-    {
-        ViewGroup root = (ViewGroup)  inflater.inflate(R.layout.login_tab_fragment, container , false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        ViewGroup root = (ViewGroup) inflater.inflate(R.layout.login_tab_fragment, container, false);
         email = root.findViewById(R.id.email);
         pass = root.findViewById(R.id.pass);
         forgetpass = root.findViewById(R.id.forgetpassword);
         login = root.findViewById(R.id.button);
 
+        firebaseAuth = FirebaseAuth.getInstance();
+
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent( root.getContext(), MainActivity.class);
-                Bundle b = new Bundle();
-                b.putString("email", String.valueOf(email.getText()));
-                b.putString("password", String.valueOf(pass.getText()));
-                intent.putExtras(b);
-                startActivity(intent);
+                firebaseAuth.createUserWithEmailAndPassword(
+                        email.getText().toString(), pass.getText().toString()
+                        )
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(getContext(), "done", Toast.LENGTH_SHORT).show();
+                                } else {
+                                    Toast.makeText(getContext(), "error", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+//                Intent intent = new Intent( root.getContext(), MainActivity.class);
+//                Bundle b = new Bundle();
+//                b.putString("email", String.valueOf(email.getText()));
+//                b.putString("password", String.valueOf(pass.getText()));
+//                intent.putExtras(b);
+//                startActivity(intent);
 
             }
         });
@@ -54,8 +78,6 @@ public class LoginTabFragment extends Fragment  {
         pass.animate().translationX(0).alpha(1).setDuration(800).setStartDelay(500).start();
         forgetpass.animate().translationX(0).alpha(1).setDuration(800).setStartDelay(500).start();
         login.animate().translationX(0).alpha(1).setDuration(800).setStartDelay(700).start();
-
-
 
 
         return root;
